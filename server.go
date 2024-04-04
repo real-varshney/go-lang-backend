2c29a5f31fc56efc465e47355c4db9c658875d69
real-varshney <109132583+real-varshney@users.noreply.github.com>
2024-04-04 21:50:48 +0530

    server.go

--- server.go
+++ server.go
@@ -9,7 +9,10 @@ import (
 	"os"
 	"sort"
 	"strings"
-
+	"log/slog"
+	"time"
+	"strconv"
+		
 	"github.com/gorilla/websocket"
 	"github.com/redis/go-redis/v9"
 )
@@ -337,9 +340,41 @@ func main() {
 	http.HandleFunc("/get-user-details", getuserdetails)
 	http.HandleFunc("/ws", handleWebSocket)
 
-	fmt.Println("Server listening on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	var cfg config
+
+	// Try to read environment variable for port (given by railway). Otherwise use default
+	port := os.Getenv("PORT")
+	intPort, err := strconv.Atoi(port)
 	if err != nil {
-		panic(err)
+		intPort = 4000
 	}
+
+	// Set the port to run the API on
+	cfg.port = intPort
+
+	// create the logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// create the application
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// create the server
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  45 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("server started", "addr", srv.Addr)
+
+	// Start the server
+	err = srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
